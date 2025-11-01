346e222d6188d7416bb094ddf1be70e9d4680776
Rom soloman <71940440+romsoloman@users.noreply.github.com>
2025-11-01 13:37:26 +0200

    Add signals module __init__.py

---
+++ Rom150Bot/signals/__init__.py
@@ -0,0 +1,5 @@
+"""Signal detection module."""
+from .entry_detector import EntrySignalDetector
+from .exit_detector import ExitSignalDetector
+
+__all__ = ['EntrySignalDetector', 'ExitSignalDetector']
